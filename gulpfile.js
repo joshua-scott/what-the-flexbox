9f3bc247d0e46565f40d108cf1befa8f90c0a4ce
Joshua Scott <jscott313@gmail.com>
2017-09-10 17:06:01 +0300

    Set up browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
 const currentFolder = '1. Navbar';
 
 gulp.task('styles', () => {
   gulp.src(`${currentFolder}/*.css`)
     .pipe(autoprefixer())
-    .pipe(gulp.dest(`${currentFolder}/build`));
+    .pipe(gulp.dest(`${currentFolder}/build`))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('watch', () => {
+gulp.task('serve', ['styles'], () => {
+
+  browserSync.init({
+    server: `${currentFolder}`
+  });
+
   gulp.watch(`${currentFolder}/*.css`, ['styles']);
+  gulp.watch(`${currentFolder}/*.html`).on('change', browserSync.reload);
 });
+
+gulp.task('default', ['serve']);
